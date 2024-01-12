ad06437a1a72ab579292761fcd22d19f56e9c6fd
Hale Arslan <75578501+fhalearslnn@users.noreply.github.com>
2024-01-12 14:36:20 +0300

    Update bookstore-api.py

--- bookstore-api.py
+++ bookstore-api.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, abort, request, make_response
 from flaskext.mysql import MySQL
 
-# Create an object named app 
+# Create an object named app ....
 app = Flask(__name__)
 
 # Configure sqlite database
